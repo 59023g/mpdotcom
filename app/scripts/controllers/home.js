661d1d0dbdaa865f458512c5f65f5d1fd2384c14
mp1erce <hell@michaelepierce.com>
2014-04-24 17:56:54 -0700

    downgrade angular, upgrade jq

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,57 +3,57 @@
 angular.module('mpApp')
   .controller('HomeCtrl', ['preloader', '$timeout', '$scope',
     function(preloader, $timeout, $scope) {
+
+      // I keep track of the state of the loading images.
+      $scope.isLoading = true;
+      $scope.isSuccessful = false;
+      $scope.percentLoaded = 0;
+      
       var bgImg = {
         images: ['images/projects/bg.png']
       };
 
       $scope.project = bgImg;
+      $scope.images = $scope.project.images;
+      console.log($scope.images);
 
-      if ($scope.project.images) {
-        $scope.images = $scope.project.images;
-        console.log($scope.images);
+      // I am the image SRC values to preload and display./
+      // --
+      // NOTE: "cache" attribute is to prevent images from caching in the
+      // browser (for the sake of the demo).
+      $scope.imageLocations = $scope.images;
 
-        // I keep track of the state of the loading images.
-        $scope.isLoading = true;
-        $scope.isSuccessful = false;
-        $scope.percentLoaded = 0;
+      // Preload the images; then, update display when returned.
+      preloader.preloadImages($scope.imageLocations).then(
+        function handleResolve() {
 
-        // I am the image SRC values to preload and display./
-        // --
-        // NOTE: "cache" attribute is to prevent images from caching in the
-        // browser (for the sake of the demo).
-        $scope.imageLocations = $scope.images;
-
-        // Preload the images; then, update display when returned.
-        preloader.preloadImages($scope.imageLocations).then(
-          function handleResolve() {
-
-            function imageLoad() {
-              $scope.isLoading = false;
-              $scope.isSuccessful = true;
-            }
-
-            $timeout(imageLoad, 250);
-            console.info('Preload Successful');
-
-          },
-          function handleReject() {
-
-            // Loading failed on at least one image.
+          function imageLoad() {
             $scope.isLoading = false;
-            $scope.isSuccessful = false;
-          },
-          function handleNotify(event) {
-
-            $scope.percentLoaded = event.percent;
-
-            console.info('Percent loaded:', event.percent);
-
+            $scope.isSuccessful = true;
           }
-        );
 
-      } else {
-        console.log('gleep');
-      }
+          $timeout(imageLoad, 250);
+          console.info('Preload Successful');
+
+        },
+        function handleReject() {
+
+          // Loading failed on at least one image.
+          $scope.isLoading = false;
+          $scope.isSuccessful = false;
+        },
+        function handleNotify(event) {
+
+          $scope.percentLoaded = event.percent;
+
+          console.info('Percent loaded:', event.percent);
+
+        }
+      );
+
+
+
+
+
     }
   ]);
